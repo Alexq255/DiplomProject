a662699a9cb56933970b1b6349c358f826738d78
Alexq255 <92524226+Alexq255@users.noreply.github.com>
2023-06-29 10:46:39 +0300

    FinalVersion2

--- app/src/main/java/com/example/practice/TovarAddClass.java
+++ app/src/main/java/com/example/practice/TovarAddClass.java
@@ -6,9 +6,6 @@ import android.widget.Toast;
 public class TovarAddClass {
     public String id,nazvanie,description,fullprice,imgTovar, warranty,Category,imgTovar1,imgTovar2;
 
-    public TovarAddClass() {
-
-    }
 
     public TovarAddClass(String id, String nazvanie, String description, String fullprice,String imgTovar,String warranty,String Category,String imgTovar1,String imgTovar2) {
         this.id = id;
